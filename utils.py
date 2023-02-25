1a4cad256164f8b27bd141c61a9bdf85595a8ab4
Shubhamxd82 <119496423+Shubhamxd82@users.noreply.github.com>
2023-02-26 00:52:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -159,6 +159,20 @@ async def broadcast_messages(user_id, message):
         return False, "Error"
     except Exception as e:
         return False, "Error"
+    
+async def groups_broadcast_messages(chat_id, message):
+    try:
+        k = await message.copy(chat_id=chat_id)
+        try:
+            await k.pin()
+        except:
+            pass
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await groups_broadcast_messages(chat_id, message)
+    except Exception as e:
+        return False, "Error"
 
 async def search_gagala(text):
     usr_agent = {
