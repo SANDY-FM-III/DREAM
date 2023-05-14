f5e4786919cef83151babf109970da34fd903c7d
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2023-05-14 08:43:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Others
 
 VERIFY = bool(environ.get('VERIFY', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gyanilinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '7bbffb78761e5fe8aa9e91bbe809e8e5a0160770')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'atglinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '12fa03d33c760854e5249b00d0ea0adff7736729')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1001818118297').split()]
 PORT = environ.get("PORT", "8080")
